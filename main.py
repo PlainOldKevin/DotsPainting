976e0817e45392c05576c9abbde67416c47187f2
unknown <kevinconwaylax@gmail.com>
2023-05-18 01:38:43 -0400

    changed dots to circles with fill, working on reposition

--- main.py
+++ main.py
@@ -15,10 +15,21 @@ def random_color():
 
 # Function to tell the cursor to draw 10 circles in a line
 def draw():
+    # 10 circles
     for _ in range(10):
+        # Choose random color
+        color = random_color()
+        # Pen down to draw
         cursor.pendown()
-        cursor.dot(20, random_color())
+        # Choose pen color, fill color, draw circle, end fill
+        cursor.pencolor(color)
+        cursor.fillcolor(color)
+        cursor.begin_fill()
+        cursor.circle(10)
+        cursor.end_fill()
+        # Pen up to move forward 
         cursor.penup()
+        # Move forward
         cursor.forward(50) 
 
 # Function to tell the cursor to reposition
@@ -27,7 +38,9 @@ def draw():
 # Setting cursor to starting position
 cursor.penup()
 cursor.setpos(-225, -125)
+cursor.speed("fastest")
 
+draw()
 
 # Create screen
 screen = t.Screen()
